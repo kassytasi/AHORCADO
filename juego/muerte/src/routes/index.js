e4d431df6cd4b9fc569b0cc85c037f1195cdf375
kassytasi <nathaa1023@gmail.com>
2025-12-05 20:45:32 -0500

    Update index.js

--- juego/muerte/src/routes/index.js
+++ juego/muerte/src/routes/index.js
@@ -3,15 +3,19 @@ import HomeView from '../views/HomeView.vue'
 import GameView from '../views/GameView.vue'
 import CategoryView from '../views/CategoryView.vue'
 import LevelView from '../views/LevelView.vue'
+import RankingView from '../views/RankingView.vue' // <-- nueva vista
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/categories', name: 'categories', component: CategoryView },
   { path: '/levels', name: 'levels', component: LevelView },
-  { path: '/game', name: 'game', component: GameView }
+  { path: '/game', name: 'game', component: GameView },
+  { path: '/ranking', name: 'ranking', component: RankingView } // <-- ruta ranking
 ]
 
 export default createRouter({
   history: createWebHistory(),
   routes
 })
+
+
